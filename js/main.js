32d2463a03bfafb3a0440f89b20f3bcdbd2e63d6
madsok <46710526+madsok@users.noreply.github.com>
2019-06-19 12:29:55 +0300

    Пока все дома

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var types = ['palace', 'flat', 'house','bungalo'];
+var map = document.querySelector('.map');
+var mapPins = map.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 var getRandomElement = function (array) {
@@ -15,8 +17,7 @@ var showElement = function (element, classHidden) {
   document.querySelector(element).classList.remove(classHidden);
 };
 
-var getAd = function () {
-  for (var i = 1; i < 9; i++);
+var getAd = function (i) {
   var ad = {
     author: {
       avatar: 'img/avatars/user0' + i + '.png'
@@ -26,8 +27,8 @@ var getAd = function () {
     },
 
     location: {
-      x: getRandomInteger(0, 1200),
-      y: getRandomInteger(130, 160)
+      x: getRandomInteger(50, 1150),
+      y: getRandomInteger(130, 630)
     }
   };
   return ad;
@@ -35,12 +36,32 @@ var getAd = function () {
 
 var getAds = function () {
   var ads = [];
-  for (var i = 0; i < 8; i++) {
-    ads[i] = getAd();
+  for (var i = 1, j = 0; i < 9; i++, j++) {
+    ads[j] = getAd(i);
   }
   return ads;
 };
 
 var ads = getAds();
 
-showElement('.map.map--faded', '.map--faded');
+var renderPin = function (ad) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style.left = ad.location.x + 'px';
+  pinElement.style.top = ad.location.y + 'px';
+  pinElement.querySelector('img').src = ad.author.avatar;
+  pinElement.querySelector('img').alt = 'Заголовок объявления';
+
+  return pinElement;
+};
+
+var renderPins = function (ads) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < ads.length; i++) {
+    fragment.appendChild(renderPin(ads[i]));
+  }
+  mapPins.appendChild(fragment);
+};
+
+renderPins(ads);
+
+showElement('.map.map--faded', 'map--faded');
