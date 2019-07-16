7fa7a31ff73b73c81a71a2e80f0ada53b98e019c
madsok <46710526+madsok@users.noreply.github.com>
2019-07-16 16:36:32 +0300

    Получение данных с сервера

---
+++ js/upload.js
@@ -0,0 +1,32 @@
+'use strict';
+(function () {
+  var GET_URL = 'https://js.dump.academy/keksobooking/data';
+  var SUCCESS_CODE = 200;
+  window.load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_CODE) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+      });
+      xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', GET_URL);
+    xhr.send();
+  }
+  var onSuccess = function (data) {
+  console.log(data);
+  };
+  window.load (onSuccess)
+})();
