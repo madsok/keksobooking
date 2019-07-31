29eb031a6c3ba19b39780128c31c014b33c6459c
madsok <46710526+madsok@users.noreply.github.com>
2019-07-31 13:22:22 +0300

    Соответствие комнат количеству гостей

--- js/form.js
+++ js/form.js
@@ -7,17 +7,32 @@
   var adFormType = document.querySelector('#type');
   var adFormTimeIn = document.querySelector('#timein');
   var adFormTimeOut = document.querySelector('#timeout');
+  var adFormRoomNumber = document.querySelector('#room_number');
+  var adFormCapacity = document.querySelector('#capacity');
   var typesMinPrices = {
     palace: 10000,
     house: 5000,
     flat: 1000,
     bungalo: 0
   };
+  var roomCapacity = {
+    '1': ['1'],
+    '2': ['2', '1'],
+    '3': ['3', '2', '1'],
+    '100': ['0']
+  };
 
   var getPriceByType = function (evt) {
     var onSelectType = typesMinPrices[evt.target.value];
     adFormPrice.min = onSelectType;
     adFormPrice.placeholder = onSelectType;
+  };
+
+  var onRoomNumberChange = function () {
+    [].forEach.call(adFormCapacity.options, function (item) {
+      item.selected = (roomCapacity[adFormRoomNumber.value][0] === item.value);
+      item.disabled = !(roomCapacity[adFormRoomNumber.value].indexOf(item.value) >= 0);
+    });
   };
 
   adFormTitle.addEventListener('invalid', function () {
@@ -51,4 +66,8 @@
   adFormTimeOut.addEventListener('change', function (evt) {
     adFormTimeIn.value = evt.target.value;
   });
+
+  onRoomNumberChange();
+  adFormRoomNumber.addEventListener('change', onRoomNumberChange);
+
 }());
